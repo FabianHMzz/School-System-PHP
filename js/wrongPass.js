5e99c613f60f96eed0e4d32abe1ce9846f506c00
Fabian Herrera Martinez <contactofabianmartinez@gmail.com>
2022-07-23 16:03:49 -0500

    alerts

---
+++ js/wrongPass.js
@@ -0,0 +1,17 @@
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+  
+  Toast.fire({
+    icon: 'error',
+    title: 'Revise sus datos',
+    text: 'Contraseña antigua incorrecta'
+  })
