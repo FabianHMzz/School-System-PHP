0e54a63242563b14d783a8cb7260c1bde8d2922f
Fabian Herrera Martinez <contactofabianmartinez@gmail.com>
2022-07-23 17:38:55 -0500

    404

---
+++ php/404.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <title>404</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="all" href="../css/404.css">
+   
+</head>
+<body>
+    <h1>¿Qué ha sucedido?</h1>
+    <img src="../img/404.png" alt="">
+    <p>La página que desea ver no ha sido encontrada</p>
+    <small>Disculpe las molestias</small>
+    <a href="../admin.php"><div class="btn from-bottom">Volver al Inicio</div></a>
+    
+
+</body>
+</html>
