b340c37b2bed7f9cf80f83bf026f035d4fdecb75
Fabian Herrera Martinez <contactofabianmartinez@gmail.com>
2022-07-26 23:14:42 -0500

    new pages

---
+++ php/reports.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>Reportes</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
+    <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet" href="../css/students.css">
+    <link rel="shortcut icon" href="../img/Instituto_Niels_Borh.png">
+    
+</head>
+
+<body>
+    <div class="container">
+        <aside>
+            <div class="top">
+                <div class="logo">
+                    <img src="../img/Instituto_Niels_Borh.png" alt="Instituto_Niels_Borh">
+                    <h2>I<span class="primary">N</span>B</h2>
+                </div>
+                <div class="close" id="close-btn">
+                    <span class="material-icons-sharp">close</span>
+                </div>
+            </div>
+            <div class="sidebar">
+                <a href="../admin.php">
+                    <span class="material-icons-sharp">grid_view</span>
+                    <h3>Dashboard</h3>
+                </a>
+                <a href="student.php">
+                    <span class="material-icons-sharp">groups</span>
+                    <h3>Alumnos</h3>
+                </a>
+                <a href="payments.php">
+                    <span class="material-icons-sharp">payments</span>
+                    <h3>Pagos</h3>
+                </a>
+                <a href="teachers.php">
+                    <span class="material-icons-sharp">person</span>
+                    <h3>Maestros</h3>
+                </a>
+                <a href="#" class="active">
+                    <span class="material-icons-sharp">auto_stories</span>
+                    <h3>Reportes</h3>
+                </a>
+                <a href="changePassword.php">
+                    <span class="material-icons-sharp">manage_accounts</span>
+                    <h3>Cambiar Contraseña</h3>
+                    </a>
+                <a href="logout.php">
+                    <span class="material-icons-sharp">logout</span>
+                    <h3>Cerrar Sesión</h3>
+                </a>
+            </div>
+        </aside>
+        <!-- Final de aside -->
+
+        <section>
+            <form action="">
+                
+            </form>
+        </section>
+
+        <div class="right">
+            <div class="top">
+                <button>
+                    <div id="menu-btn">
+                        <span class="material-icons-sharp">menu</span>
+                    </div>
+                </button>
+                <div class="theme-toggle">
+                    <span class="material-icons-sharp active">light_mode</span>
+                    <span class="material-icons-sharp">dark_mode</span>
+                </div>
+                <div class="profile">
+                    <div class="info">
+                        <p>Hey, <b>Fabian</b></p>
+                        <small class="text-muted">Admin</small>
+                    </div>
+                    <div class="profile-photo">
+                        <img src="../img/Instituto_Niels_Borh.png" width="37px" alt="">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <script src="../js/script.js"></script>
+</body>
+
+</html>
